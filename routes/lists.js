949e75236e1b7f1653010c304a98592939570a3d
Trần Hữu Toàn <jungleoffice02@gmail.com>
2022-05-02 12:22:39 +0700

    make it better

--- routes/lists.js
+++ routes/lists.js
@@ -32,7 +32,7 @@ router.delete("/:id", verify, async (req, res) => {
 });
 
 // Get
-router.get("/", verify, async (req, res) => {
+router.get("/", async (req, res) => {
   const typeQuery = req.query.type;
   const genreQuery = req.query.genre;
   let list = [];
